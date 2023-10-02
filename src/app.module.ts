7750133c95d24d2953c1f9eeaa9ecb0dbde5d3b3
yukebrillianth <yukebrilliant@gmail.com>
2023-10-03 00:03:55 +0700

    remove throttle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
-    ThrottlerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => [
-        {
-          ttl: configService.get<number>('THROTTLER_TTL_SECONDS'),
-          limit: configService.get<number>('THROTTLER_LIMIT'),
-        },
-      ],
-    }),
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Ip, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { ActivationDTO } from './dto/activation.dto';
 import { AuthService } from './auth.service';
-import { Throttle } from '@nestjs/throttler';
 import { RegisterDTO } from './dto';
 
 @Controller({
@@ -20,9 +19,9 @@ export class AuthController {
     return this.authService.register(data);
   }
 
-  @Throttle({ default: { limit: 3, ttl: 300 } })
   @Post('verify/email/request')
-  emailVerification(@Body() data: ActivationDTO) {
+  emailVerification(@Body() data: ActivationDTO, @Ip() ip: number) {
+    console.log(ip);
     return this.authService.emailVerificationRequest(data);
   }
 }
